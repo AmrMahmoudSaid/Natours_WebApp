0cc9d512da323d6903efa2fb485f68dbca4bd01c
amr mahmoud <amrmahmoud1900@gmail.com>
2023-03-07 15:48:34 +0200

    add /me endpoint

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -1,6 +1,9 @@
 const dotenv = require('dotenv');
 const  fs = require('fs');
 const tour= require('../../models/tourModel');
+const user= require('../../models/userModel');
+const review= require('../../models/reviewModel');
+
 dotenv.config({path : './config.env'})
 const  mongoose = require('mongoose')
 // console.log((process.env));
@@ -12,9 +15,14 @@ mongoose.connect(DB,{
     useUnifiedTopology : true
 });
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`,'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`,'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`,'utf-8'));
+
 const importData =async ()=>{
     try{
         await tour.create(tours);
+        await user.create(users , {validateBeforeSave : false});
+        await review.create(reviews);
         console.log('Data successfully loaded');
         process.exit();
     }catch (err){
@@ -24,6 +32,9 @@ const importData =async ()=>{
 const deleteData = async ()=>{
     try{
        await tour.deleteMany();
+        await user.deleteMany();
+        await review.deleteMany();
+
         console.log('Data successfully deleted');
         process.exit();
     }catch (err){
