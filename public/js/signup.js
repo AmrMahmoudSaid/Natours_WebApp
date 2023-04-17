162d46c2162c51d2de8cf1f05f9ace3cec5f64ee
amr mahmoud <amrmahmoud1900@gmail.com>
2023-04-17 17:00:21 +0200

    add signup feature

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+//to make new features work in older browsers
+import '@babel/polyfill'
+import  axios from 'axios';
+import {showAlert} from "./alert";
+
+export const signup = async (name,email,password,passwordConfirm) =>{
+    try{
+        const res =await axios({
+            method: 'POST' ,
+            url: 'http://127.0.0.1:3000/api/v1/users/signup',
+            data : {
+                name,
+                email,
+                password,
+                passwordConfirm
+            }
+        });
+
+        if (res.data.status==='success'){
+            showAlert('success','sign up in successfully');
+            window.setTimeout(()=>{
+                location.assign('/me');
+            },500)
+        }
+        console.log(res);
+    }catch (err) {
+        showAlert('error',err.response.data.message);
+    }
+}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/' ,authController.isLoggedIn,viewController.getOverview);
 router.get('/tour/:slug',authController.protect ,viewController.getTour);
 router.get('/login',authController.isLoggedIn,viewController.getLoginForm);
-router.get('/singup',authController.isLoggedIn,viewController.getSingupForm);
+router.get('/signup',viewController.getSingupForm);
 router.get('/me',authController.protect ,viewController.getAccount);
 router.post('/submit-user-data',authController.protect,viewController.updataUserData)
 module.exports = router;
